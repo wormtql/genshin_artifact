e577e031696b80c3f8ad266fa9b91102ac56c81a
wormtql <584130248@qq.com>
2021-03-03 23:02:30 +0800

    feat(Hutao): update Hutao target function
    able to customize Melt and Evaporate ratio
    able to customize Heavy Attack frequency

--- src/assets/target_functions/data/hutao/HutaoConfig.vue
+++ src/assets/target_functions/data/hutao/HutaoConfig.vue
@@ -4,6 +4,26 @@
             <h3 class="config-title">生命值低于50%</h3>
             <el-switch v-model="hpBelow50" active-text="是" inactive-text="否"></el-switch>
         </div>
+
+        <div class="config-item">
+            <h3 class="config-title">敌方火元素覆盖率</h3>
+            <el-input v-model="pyroRate"></el-input>
+        </div>
+
+        <div class="config-item">
+            <h3 class="config-title">蒸发占比（0-1）</h3>
+            <el-input v-model="evaporate"></el-input>
+        </div>
+
+        <div class="config-item">
+            <h3 class="config-title">融化占比（0-1）（请确保与蒸发占比之和小于1）</h3>
+            <el-input v-model="melt"></el-input>
+        </div>
+
+        <div class="config-item">
+            <h3 class="config-title">重击频率</h3>
+            <el-input v-model="bFreq"></el-input>
+        </div>
     </div>
 </template>
 
@@ -13,6 +33,21 @@ export default {
     data: function () {
         return {
             hpBelow50: false,
+            pyroRate: "0.9",
+            evaporate: "0.5",
+            melt: "0",
+            bFreq: "0.7",
+        }
+    },
+    methods: {
+        compact() {
+            return {
+                hpBelow50: this.hpBelow50,
+                pyroRate: parseFloat(this.pyroRate),
+                evaporate: parseFloat(this.evaporate),
+                melt: parseFloat(this.melt),
+                bFreq: parseFloat(this.bFreq),
+            };
         }
     }
 }
--- src/assets/target_functions/data/hutao/hutaoE.js
+++ src/assets/target_functions/data/hutao/hutaoE.js
@@ -7,28 +7,57 @@ import skill from "./skill";
 function f(config) {
     let baseAtk = config.character.baseAtk + config.weapon.baseAtk;
     let eLevel = config.cArgs.skill2;
+    let aLevel = config.cArgs.skill1;
     let atkInc = skill.e.hp[eLevel - 1];
+
+    let aT = skill.a.dmg3[aLevel - 1];
+    let bT = skill.b.dmg[aLevel - 1];
 
     let hasTalent2 = config.character.hasTalent2;
 
     let hpBelow50 = config.tArgs.hpBelow50;
+    let pyroRate = config.tArgs.pyroRate;
+    let evaporate = config.tArgs.evaporate;
+    let melt = config.tArgs.melt;
+    let normal = 1 - evaporate - melt;
+    let bFreq = config.tArgs.bFreq;
+    let aFreq = 1 - bFreq;
+    //console.log(aFreq, bFreq);
+    //console.log(aT, bT);
+    // console.log(evaporate, melt, normal);
+
     let talentBonus = (hasTalent2 && hpBelow50) ? 0.33 : 0;
 
     return function (attribute, context) {
         let isCW4 = (context.artifactSet.crimsonWitch || 0) >= 4;
+        let isLW4 = (context.artifactSet.lavaWalker || 0) >= 4;
 
         let atkBonus = attribute.life() * atkInc;
         atkBonus = Math.min(atkBonus, 4 * baseAtk);
 
         let atk = attribute.attack() + atkBonus;
-        let bonus = attribute.aBonus + attribute.bonus + attribute.fireBonus + talentBonus;
+        let commonBonus = attribute.bonus + attribute.fireBonus + talentBonus;
+
+        let em = attribute.elementalMastery;
+        let amp = 20 * em / (3 * (em + 1400));
         if (isCW4) {
-            bonus += 0.075;
+            commonBonus += 0.075;
+            amp += 0.15;
+        }
+        if (isLW4) {
+            commonBonus += pyroRate * 0.35;
         }
 
         let crit = Math.min(1, attribute.critical);
 
-        return atk * (1 + bonus) * (1 + crit * attribute.criticalDamage);
+        let a = atk * (1 + commonBonus + attribute.aBonus) * aFreq * aT;
+        let b = atk * (1 + commonBonus + attribute.bBonus) * bFreq * bT;
+
+        return (a + b) * (1 + crit * attribute.criticalDamage) * (normal + (evaporate * 1.5 + melt * 2) * (1 + amp));
+
+        // return atk * (1 + bonus) * (1 + crit * attribute.criticalDamage) * (normal + (evaporate * 1.5 + melt * 2) * (1 + amp))
+        //     * (aFreq * aT + bFreq * bT)
+        // ;
     };
 }
 
