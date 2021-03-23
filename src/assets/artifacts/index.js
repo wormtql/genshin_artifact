7645b7dfd58c82ce1ad236d61aa6a2de7cc089ad
碎碎酱 <yinxin630@gmail.com>
2021-03-23 20:46:02 +0800

    feat: support to paste image and read data

--- src/assets/artifacts/index.js
+++ src/assets/artifacts/index.js
@@ -17,3 +17,39 @@ export const artifactsData = cache;
 export const artifactsTagMap = _artifactsTagMap;
 export const artifactsSecondaryTag = _artifactsSecondaryTag;
 export const artifactsIcon = _artifactsIcon;
+
+export const artifactsNamesSet = new Set();
+export const artifactsNameTypeMap = {};
+
+(() => {
+    for (const artifact of Object.values(cache)) {
+        if (artifact.flower) {
+            artifactsNamesSet.add(artifact.flower.chs);
+            artifactsNameTypeMap[artifact.flower.chs] = artifact.eng;
+        }
+        if (artifact.feather) {
+            artifactsNamesSet.add(artifact.feather.chs);
+            artifactsNameTypeMap[artifact.feather.chs] = artifact.eng;
+        }
+        if (artifact.sand) {
+            artifactsNamesSet.add(artifact.sand.chs);
+            artifactsNameTypeMap[artifact.sand.chs] = artifact.eng;
+        }
+        if (artifact.cup) {
+            artifactsNamesSet.add(artifact.cup.chs);
+            artifactsNameTypeMap[artifact.cup.chs] = artifact.eng;
+        }
+        if (artifact.head) {
+            artifactsNamesSet.add(artifact.head.chs);
+            artifactsNameTypeMap[artifact.head.chs] = artifact.eng;
+        }
+    }
+})();
+
+export const artifactsPositionTypeMap = {
+    '生之花': 'flower',
+    '死之羽': 'feather',
+    '时之沙': 'sand',
+    '空之杯': 'cup',
+    '理之冠': 'head',
+}
