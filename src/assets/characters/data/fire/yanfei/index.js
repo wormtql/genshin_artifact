9aad9ffc821fc8511c01f3e136a151f6110a32d1
wormtql <584130248@qq.com>
2021-04-29 20:22:13 +0800

    feat(characters): Add *Yanfei*
    - Change artifacts images to transparent png

---
+++ src/assets/characters/data/fire/yanfei/index.js
@@ -0,0 +1,12 @@
+import card from "./card.jpg";
+import avatar from "@asset/badges/yanfei.png";
+
+export default {
+    cardURL: card,
+    name: "yanfei",
+    chs: "烟绯",
+    element: "fire",
+    weapon: "book",
+    star: 4,
+    avatar,
+}
