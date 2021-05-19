5d4e180f83a4c98e1315b37090fac105acb6c0e1
wormtql <584130248@qq.com>
2021-05-19 14:02:58 +0800

    feat: Add 优菈 and 松籁响起之时
    eula
    song of broken pines

---
+++ src/assets/characters/data/ice/youla/index.js
@@ -0,0 +1,12 @@
+import card from "./card.png";
+import avatar from "@asset/badges/youla.png";
+
+export default {
+    cardURL: card,
+    name: "youla",
+    chs: "优菈",
+    element: "ice",
+    weapon: "sword2",
+    star: 5,
+    avatar,
+}
