559f28dcae8da54a7a08eb3f33ea121c3bd200ee
wormtql <584130248@qq.com>
2021-06-30 16:55:21 +0800

    feat: Add 枫原万叶 and 苍古自由之誓

---
+++ src/assets/characters/data/wind/枫原万叶/index.js
@@ -0,0 +1,14 @@
+import card from "./card.png";
+import avatar from "@asset/badges/fengyuanwanye.png";
+import skill from "./fengyuanwanye.skill.js";
+
+export default {
+    cardURL: card,
+    name: "fengyuanwanye",
+    chs: "枫原万叶",
+    element: "wind",
+    weapon: "sword",
+    star: 5,
+    avatar,
+    skill,
+}
