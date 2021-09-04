68ee8c6fe75d49efebd90b742290f9794be41169
wormtql <584130248@qq.com>
2021-09-04 17:06:09 +0800

    chore(targetfuncs): Modify Ayaka target func descriptions

--- src/assets/target_functions/data/ice/shenlilinghua/shenlilinghuaDps.tf.js
+++ src/assets/target_functions/data/ice/shenlilinghua/shenlilinghuaDps.tf.js
@@ -3,14 +3,15 @@ import config from "./shenlilinghuaDps.tcfg";
 
 export default {
     name: "shenlilinghuaDps",
-    chs: "白鹭公主-神里绫华",
+    chs: "神里绫华-白鹭公主",
     description: [
         "期望 DPS 输出, 输出手法模拟如下循环: 4s 左右辅助铺场, 平a4段接重击，有e放e，有大开大",
+        "该函数由（https://github.com/linwe2012）提供",
     ],
     tags: [
         "输出",
         "神里绫华",
-        "战场",
+        "站场",
         "Dps",
     ],
     "for": "shenlilinghua",
