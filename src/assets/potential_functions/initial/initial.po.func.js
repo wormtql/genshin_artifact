34185243213d8069995ae60add03a5f6b8fe8e2f
Jeffery You <jyou@adobe.com>
2021-11-29 23:35:07 -0800

    fixed a bug of getting wrong score when there are 3 normal tags

--- src/assets/potential_functions/initial/initial.po.func.js
+++ src/assets/potential_functions/initial/initial.po.func.js
@@ -7,7 +7,7 @@ import {
 
 function f() {
     return function(artifact) {
-        if (artifact.level > 4 || artifact.star !== 5) {
+        if (artifact.level > 7 || artifact.star !== 5) {
             return 0;
         }
         let mainTag = artifact.mainTag.name;
@@ -15,18 +15,24 @@ function f() {
             mainTag = elementalBonusMap[mainTag];
         }
         const position = artifact.position;
+        let normalTags = [...artifact.normalTags];
+        if (normalTags.length === 3) {
+            normalTags.push({ name: 'blank', value: 0 });
+        }
         const scores = classTypes.map(classType => {
             let score = 0;
             if (mainTagRatio[position][mainTag][classType.name]) {
-                score = artifact.normalTags.map(normalTag => {
+                score = normalTags.map(normalTag => {
                     return normalTagRatio[normalTag.name][classType.name];
-                }).
-                    reduce((previousValue, currentValue) => previousValue +
-                        currentValue, score);
+                }).reduce(
+                    (previousValue, currentValue) => previousValue +
+                        currentValue,
+                    score,
+                );
                 score = classType.ratio *
                     (score + mainTagRatio[position][mainTag][classType.name]);
             }
-            return score;
+            return score.toFixed(2);
         });
 
         return Math.max(...scores);
