3d35ca4637dcbdbad6210ac37316170299ffb377
Yokami <95584793+kawayiYokami@users.noreply.github.com>
2022-02-17 16:49:05 +0800

    添加八重神子的带反应E期望
    反应的时候区别还挺大的，默认是对90级怪物

---
+++ src/assets/target_functions/data/thunder/bachongshenzi/bachongE_reaction.tcfg.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div class="config-item">
+            <h3 class="config-title">反应类型</h3>
+            <el-radio-group
+                v-model="mode"
+                size="small"
+            >
+                <el-radio-button label="Overloaded">超载</el-radio-button>
+                <el-radio-button label="ElectroCharged">感电</el-radio-button>
+            </el-radio-group>
+        </div>
+
+        <div
+            class="config-item"
+            v-if="mode === 'Overloaded'"
+        >
+            <h3 class="config-title">一轮3E中触发次数</h3>
+            <el-input
+                v-model="Overloadedtimes"
+                size="small"
+            ></el-input>
+        </div>
+
+        <div
+            class="config-item"
+            v-if="mode === 'ElectroCharged'"
+        >
+            <h3 class="config-title">一轮3E中触发次数</h3>
+            <el-input
+                v-model="electroChargedTimes"
+                size="small"
+            ></el-input>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "bachongE_reaction.tcfg",
+    data() {
+        return {
+            Overloadedtimes: "5.0",
+            electroChargedTimes: "8.0",
+            mode: "Overloaded",
+        }
+    },
+    methods: {
+        compact() {
+            return {
+                Overloadedtimes:parseFloat(this.Overloadedtimes) ?? 5.0,
+                electroChargedTimes:parseFloat(this.electroChargedTimes) ?? 8.0,
+                mode: this.mode,
+            };
+        },
+        setData(d) {
+            this.Overloadedtimes = d.Overloadedtimes.toString();
+            this.electroChargedTimes = d.electroChargedTimes.toString();
+            this.mode = d.mode;
+        }
+    }
+    
+}
+</script>
---
+++ src/assets/target_functions/data/thunder/bachongshenzi/bachongE_reaction.tf.js
@@ -0,0 +1,16 @@
+import badge from "@asset/badges/bachongshenzi.png";
+import config from "./bachongE_reaction.tcfg.vue";
+
+export default {
+    name: "bachongE_reaction",
+    chs: "八重神子-杀生樱-带剧变反应 总伤害最高 by 遥酱",
+    description: [
+        "八重神子-杀生樱-带剧变反应 总伤害最高",
+    ],
+    tags: [
+        "反应输出总期望"
+    ],
+    "for": "bachongshenzi",
+    badge,
+    config,
+}
