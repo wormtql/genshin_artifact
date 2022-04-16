e0f90d90ba8a9ad739b732e4a24d4c73608f9d38
wormtql <584130248@qq.com>
2022-04-16 20:42:59 +0800

    feat: Send compute feedback to server

---
+++ src/api/misc.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+import { client } from "./client"
+
+
+export async function createFeedback(feedback) {
+    return await client.post("/feedbacks/create", {
+        text: feedback
+    })
+}
+
+export async function createComputeResult(characterInterface, weaponInterface, buffsInterface, targetFunctionInterface, resultArtifacts) {
+    const config = {
+        character: characterInterface,
+        weapon: weaponInterface,
+        buffs: buffsInterface,
+        target_function: targetFunctionInterface
+    }
+
+    const data = {
+        config,
+        result_artifacts: resultArtifacts
+    }
+
+    return await client.post("/compute_result/create", data)
+}
