fdcd9952614722751218fc61b86f108412d93253
wormtql <584130248@qq.com>
2022-04-19 19:31:29 +0800

    feat: Add more levels

--- src/components/select/SelectWeaponLevel.vue
+++ src/components/select/SelectWeaponLevel.vue
@@ -4,10 +4,22 @@
         @input="$emit('input', $event)"
         size="small"
     >
-        <el-option label="60突破" value="60+"></el-option>
-        <el-option label="70突破" value="70+"></el-option>
-        <el-option label="80突破" value="80+"></el-option>
-        <el-option label="90" value="90-"></el-option>
+        <el-option-group label=">=60">
+            <el-option label="60-" value="60-"></el-option>
+            <el-option label="60+" value="60+"></el-option>
+            <el-option label="65" value="65"></el-option>
+        </el-option-group>
+        <el-option-group label=">=70">
+            <el-option label="70-" value="70-"></el-option>
+            <el-option label="70+" value="70+"></el-option>
+            <el-option label="75" value="75"></el-option>
+        </el-option-group>
+        <el-option-group label=">=80">
+            <el-option label="80-" value="80-"></el-option>
+            <el-option label="80+" value="80+"></el-option>
+            <el-option label="85" value="85"></el-option>
+            <el-option label="90" value="90-"></el-option>
+        </el-option-group>
     </el-select>
 </template>
 
