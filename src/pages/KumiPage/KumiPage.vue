9ad98aac5ed2f36a972924471d0899b4d1929299
wormtql <584130248@qq.com>
2022-04-21 19:30:51 +0800

    feat: Add responsive to kumi page

--- src/pages/KumiPage/KumiPage.vue
+++ src/pages/KumiPage/KumiPage.vue
@@ -1,10 +1,5 @@
 <template>
     <div>
-        <el-breadcrumb>
-            <el-breadcrumb-item>圣遗物组</el-breadcrumb-item>
-        </el-breadcrumb>
-        <el-divider></el-divider>
-
         <el-dialog
             :visible.sync="showSelectArtifactDialog"
             title="选择圣遗物"
@@ -16,27 +11,63 @@
             ></select-artifact>
         </el-dialog>
 
-        <el-row :gutter="16">
-            <el-col :span="4" class="left">
+        <div class="m-toolbar hidden-md-and-up">
+            <el-dropdown trigger="click" @command="handleCommand">
+                <span><i class="el-icon-more"></i></span>
+
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="add"><i class="el-icon-plus"></i>新建收藏夹</el-dropdown-item>
+                    <el-dropdown-item command="addKumi"><i class="el-icon-plus"></i>新建圣遗物组</el-dropdown-item>
+                    <el-dropdown-item command="delete" v-if="currentDirId !== 0"><i class="el-icon-delete"></i>删除"{{ currentDirTitle }}"</el-dropdown-item>
+                    <el-dropdown-item command="rename" v-if="currentDirId !== 0"><i class="el-icon-edit"></i>重命名</el-dropdown-item>
+                    <el-dropdown-item
+                        v-for="(item, index) in items"
+                        :key="item.index"
+                        :divided="index === 0"
+                        :command="'use ' + item.index"
+                    ><i class="el-icon-menu"></i>{{ item.title }}</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+
+            <div class="m-middle">
+                {{ currentDirTitle }}
+            </div>
+        </div>
+
+        <div class="pc-container">
+            <div class="left mona-scroll-hidden hidden-sm-and-down">
                 <div class="toolbar">
-                    <my-button1
-                        icon="el-icon-plus"
-                        @click="handleNewDir"
-                        title="新建收藏夹"
-                    ></my-button1>
+                    <el-button size="mini" type="primary" icon="el-icon-plus" @click="handleNewDir">
+                        新建收藏夹
+                    </el-button>
+<!--                    <my-button1-->
+<!--                        icon="el-icon-plus"-->
+<!--                        @click="handleNewDir"-->
+<!--                        title="新建收藏夹"-->
+<!--                    ></my-button1>-->
                 </div>
 
-                <div class="mona-scroll"
-                    :style="{ height: contentHeight }"
-                >
+                <div class="mona-scroll-hidden left-list">
                     <my-list
                         v-model="currentDirId"
                         :items="items"
                     ></my-list>
                 </div>
-            </el-col>
-            <el-col :span="20" class="right-col">
-                <div class="toolbar2">
+            </div>
+            <div class="right mona-scroll-hidden">
+                <div class="hidden-md-and-up">
+                    <el-input
+                        v-model="searchString"
+                        size="mini"
+                        style="margin-bottom: 12px"
+                        placeholder="搜索"
+                    >
+                        <template #append>
+                            <i class="el-icon-search"></i>
+                        </template>
+                    </el-input>
+                </div>
+                <div class="toolbar2 hidden-sm-and-down">
                     <div class="button-left">
                         <el-button
                             size="mini"
@@ -44,11 +75,6 @@
                             icon="el-icon-plus"
                             @click="handleNewKumi"
                         >新建组</el-button>
-<!--                        <my-button1-->
-<!--                            icon="el-icon-plus"-->
-<!--                            @click="handleNewKumi"-->
-<!--                            title="新建圣遗物组"-->
-<!--                        ></my-button1>-->
                         <el-input
                             v-model="searchString"
                             style="margin-left: 16px"
@@ -69,12 +95,6 @@
                             v-if="currentDirId !== 0"
                             title="删除收藏夹"
                         ><i class="el-icon-delete"></i></el-button>
-<!--                        <my-button1-->
-<!--                            v-if="currentDirId !== 0"-->
-<!--                            icon="el-icon-edit"-->
-<!--                            @click="handleRenameDir(currentDirId)"-->
-<!--                            title="重命名收藏夹"-->
-<!--                        ></my-button1>-->
                         <el-button
                             v-if="currentDirId !== 0"
                             size="mini"
@@ -84,9 +104,7 @@
                     </div>
                 </div>
 
-                <div class="items mona-scroll" ref="content"
-                    :style="{ height: contentHeight }"
-                >
+                <div class="items mona-scroll-hidden">
                     <div v-if="filteredKumiList.length > 0">
                         <kumi-item
                             v-for="kumi in filteredKumiList"
@@ -102,29 +120,98 @@
                         <el-empty></el-empty>
                     </div>
                 </div>
-<!--                <kumi-directory-display-->
-<!--                    :dirId="currentDirId"-->
-<!--                    @delete="handleDeleteDir"-->
-<!--                ></kumi-directory-display>-->
-<!--                {{ currentKumiList }}-->
-            </el-col>
-        </el-row>
+            </div>
+        </div>
+
+<!--        <el-row :gutter="16" style="height: 100%">-->
+<!--            <el-col :span="4" class="left hidden-sm-and-down">-->
+<!--                -->
+<!--            </el-col>-->
+<!--            <el-col :sm="24" :md="20" class="right-col">-->
+<!--                <div class="hidden-md-and-up">-->
+<!--                    <el-input-->
+<!--                        v-model="searchString"-->
+<!--                        size="mini"-->
+<!--                        style="margin-bottom: 12px"-->
+<!--                        placeholder="搜索"-->
+<!--                    >-->
+<!--                        <template #append>-->
+<!--                            <i class="el-icon-search"></i>-->
+<!--                        </template>-->
+<!--                    </el-input>-->
+<!--                </div>-->
+<!--                <div class="toolbar2 hidden-sm-and-down">-->
+<!--                    <div class="button-left">-->
+<!--                        <el-button-->
+<!--                            size="mini"-->
+<!--                            type="primary"-->
+<!--                            icon="el-icon-plus"-->
+<!--                            @click="handleNewKumi"-->
+<!--                        >新建组</el-button>-->
+<!--                        <el-input-->
+<!--                            v-model="searchString"-->
+<!--                            style="margin-left: 16px"-->
+<!--                            size="mini"-->
+<!--                            placeholder="搜索"-->
+<!--                        >-->
+<!--                            <template #append>-->
+<!--                                <i class="el-icon-search"></i>-->
+<!--                            </template>-->
+<!--                        </el-input>-->
+<!--                    </div>-->
+
+<!--                    <div class="button-right">-->
+<!--                        <el-button-->
+<!--                            size="mini"-->
+<!--                            type="danger"-->
+<!--                            @click="handleDeleteDir(currentDirId)"-->
+<!--                            v-if="currentDirId !== 0"-->
+<!--                            title="删除收藏夹"-->
+<!--                        ><i class="el-icon-delete"></i></el-button>-->
+<!--                        <el-button-->
+<!--                            v-if="currentDirId !== 0"-->
+<!--                            size="mini"-->
+<!--                            icon="el-icon-edit"-->
+<!--                            @click="handleRenameDir(currentDirId)"-->
+<!--                        >重命名</el-button>-->
+<!--                    </div>-->
+<!--                </div>-->
+
+<!--                <div class="items mona-scroll-hidden">-->
+<!--                    <div v-if="filteredKumiList.length > 0">-->
+<!--                        <kumi-item-->
+<!--                            v-for="kumi in filteredKumiList"-->
+<!--                            :key="kumi.id"-->
+<!--                            :data="kumi"-->
+<!--                            @delete="handleDeleteKumi(kumi.id)"-->
+<!--                            @edit="handleEditKumi(kumi.id)"-->
+<!--                            @click="handleClickPosition(kumi.id, $event)"-->
+<!--                            @deleteArtifact="handleDeleteArtifact(kumi.id, $event)"-->
+<!--                        ></kumi-item>-->
+<!--                    </div>-->
+<!--                    <div v-else>-->
+<!--                        <el-empty></el-empty>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </el-col>-->
+<!--        </el-row>-->
         
     </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex"
+import {mapGetters} from "vuex"
 import {
+    checkKumiName,
+    checkName,
+    deleteDir,
+    deleteKumi,
     getKumisByDir,
     newDir,
-    checkName,
-    checkKumiName,
     newKumi,
-    deleteKumi,
+    removeKumiArtifact,
     renameItem,
-    deleteDir,
-    updateKumiArtifact, removeKumiArtifact
+    updateKumiArtifact
 } from "@util/kumi"
 import Fuse from "fuse.js"
 
@@ -141,13 +228,6 @@ export default {
         KumiItem,
         SelectArtifact
     },
-    mounted() {
-        const content = this.$refs["content"]
-        const rect = content.getBoundingClientRect()
-
-        this.contentHeight = `calc(100vh - ${rect.top}px)`
-        // console.log(rect)
-    },
     data() {
         return {
             currentDirId: 0,
@@ -156,7 +236,6 @@ export default {
             selectArtifactPosition: "flower",
             underSelectionKumiId: 0,
 
-            contentHeight: "100px",
             searchString: ""
         }
     },
@@ -175,6 +254,16 @@ export default {
                 })
             }
             return temp
+        },
+
+        currentDirTitle() {
+            for (let dir of this.directories) {
+                if (dir.id === this.currentDirId) {
+                    return dir.title
+                }
+            }
+
+            return ""
         },
 
         currentKumiList() {
@@ -196,6 +285,20 @@ export default {
         }
     },
     methods: {
+        handleCommand(command) {
+            if (command === "add") {
+                this.handleNewDir()
+            } else if (command.startsWith("use")) {
+                this.currentDirId = parseInt(command.split(" ")[1])
+            } else if (command === "delete") {
+                this.handleDeleteDir(this.currentDirId)
+            } else if (command === "rename") {
+                this.handleRenameDir(this.currentDirId)
+            } else if (command === "addKumi") {
+                this.handleNewKumi()
+            }
+        },
+
         // dir
         handleNewDir() {
             this.getInputDirName("新建", "收藏夹名").then(name => {
@@ -310,6 +413,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.m-toolbar {
+    margin-bottom: 12px;
+    position: relative;
+    .m-middle {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        color: #606166;
+        font-size: 12px;
+    }
+}
+
 .toolbar {
     margin-bottom: 16px;
     display: flex;
@@ -328,19 +444,42 @@ export default {
     }
 }
 
-.right-col {
-    //display: flex;
-    //flex-direction: column;
-    //
-    //.items {
-    //    flex-grow: 1;
-    //}
-
+.left {
+    height: 100%;
 }
 
-.items {
-    overflow: auto;
-    padding-right: 20px;
+@media only screen and (max-width: 992px) {
+    .right-col {
+        height: 100vh;
+    }
+}
+//.items {
+//    overflow: auto;
+//    padding-right: 20px;
+//}
+
+@media only screen and (max-width: 992px) {
+    .right {
+
+    }
+}
+
+@media only screen and (min-width: 992px) {
+    .pc-container {
+        display: flex;
+        height: 100%;
+        align-items: flex-start;
+
+        .left {
+            width: 200px;
+            height: calc(100vh - 2 * 24px);
+        }
+
+        .right {
+            width: calc(100% - 150px);
+            height: calc(100vh - 2 * 24px);
+        }
+    }
 }
 
 /*.left {*/
