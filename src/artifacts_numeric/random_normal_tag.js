96b4b36cb07ad4e7cdc8d1cefb1d8574f898f554
wormtql <584130248@qq.com>
2022-07-01 11:12:39 +0800

    fix: Random artifact sub stat

--- src/artifacts_numeric/random_normal_tag.js
+++ src/artifacts_numeric/random_normal_tag.js
@@ -1,18 +1,19 @@
-import attribute from "@const/artifact_eff";
+// import attribute from "@const/artifact_eff";
 import { random01, randomIntBetween, randomElement } from "@util/random";
-import { artifactsSecondaryTag as tags } from "@asset/artifacts";
+// import { artifactsSecondaryTag as tags } from "@asset/artifacts";
+import {artifactTags as tags, subStats, artifactEff} from "@/constants/artifact"
 
 function randomNormalTag(count, exclude) {
-    let canditate = tags.filter(tag => exclude.indexOf(tag) === -1);
-    let len = canditate.length;
+    let candidate = subStats.filter(tag => exclude.indexOf(tag) === -1);
+    let len = candidate.length;
     let ans = [];
     for (let i = 0; i < count; i++) {
         let index = randomIntBetween(0, len);
-        ans.push(canditate[index]);
+        ans.push(candidate[index]);
 
-        let temp = canditate[index];
-        canditate[index] = canditate[len - 1];
-        canditate[len - 1] = temp;
+        let temp = candidate[index];
+        candidate[index] = candidate[len - 1];
+        candidate[len - 1] = temp;
 
         len--;
     }
@@ -21,12 +22,12 @@ function randomNormalTag(count, exclude) {
 }
 
 export default function (star, level, exclude) {
-    let attr = attribute[star];
+    let attr = artifactEff[star];
 
     let upCount = Math.floor(level / 4);
-    let initalCount = star - 2 + random01();
+    let initialCount = star - 2 + random01();
 
-    let tagCount = Math.min(4, upCount + initalCount);
+    let tagCount = Math.min(4, upCount + initialCount);
     let normalTags = randomNormalTag(tagCount, exclude);
     // console.log(normalTags);
 
@@ -38,7 +39,7 @@ export default function (star, level, exclude) {
         });
     });
 
-    let remain = upCount + initalCount - 4;
+    let remain = upCount + initialCount - 4;
     for (let i = 0; i < remain; i++) {
         let index = randomIntBetween(0, ret.length);
         ret[index].value += randomElement(attr[ret[index].name]);
