39643e1b7886faf57d2a197b6f358c12adae8ddd
CorinthusYu <736674365@qq.com>
2023-10-03 17:33:35 +0800

    opt: better wrio config & tf

--- src/assets/_gen_pf.js
+++ src/assets/_gen_pf.js
@@ -12,45 +12,45 @@ export default {
         badge: ArtifactEff_image,
         config: [
             
-            {"default":false,"name":"atk_use","title":730,"type":"bool"},
+            {"default":false,"name":"atk_use","title":732,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"atk_weight","title":731,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"atk_weight","title":733,"type":"float"},
             
-            {"default":true,"name":"atk_p_use","title":722,"type":"bool"},
+            {"default":true,"name":"atk_p_use","title":724,"type":"bool"},
             
-            {"default":1.0,"max":1.0,"min":0.0,"name":"atk_p_weight","title":723,"type":"float"},
+            {"default":1.0,"max":1.0,"min":0.0,"name":"atk_p_weight","title":725,"type":"float"},
             
-            {"default":false,"name":"hp_use","title":1277,"type":"bool"},
+            {"default":false,"name":"hp_use","title":1279,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"hp_weight","title":1278,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"hp_weight","title":1280,"type":"float"},
             
-            {"default":false,"name":"hp_p_use","title":1262,"type":"bool"},
+            {"default":false,"name":"hp_p_use","title":1264,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"hp_p_weight","title":1263,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"hp_p_weight","title":1265,"type":"float"},
             
-            {"default":false,"name":"def_use","title":1741,"type":"bool"},
+            {"default":false,"name":"def_use","title":1743,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"def_weight","title":1742,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"def_weight","title":1744,"type":"float"},
             
-            {"default":false,"name":"def_p_use","title":1736,"type":"bool"},
+            {"default":false,"name":"def_p_use","title":1738,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"def_p_weight","title":1737,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"def_p_weight","title":1739,"type":"float"},
             
-            {"default":true,"name":"critical_use","title":995,"type":"bool"},
+            {"default":true,"name":"critical_use","title":997,"type":"bool"},
             
-            {"default":1.0,"max":1.0,"min":0.0,"name":"critical_weight","title":996,"type":"float"},
+            {"default":1.0,"max":1.0,"min":0.0,"name":"critical_weight","title":998,"type":"float"},
             
-            {"default":true,"name":"critical_damage_use","title":989,"type":"bool"},
+            {"default":true,"name":"critical_damage_use","title":991,"type":"bool"},
             
-            {"default":1.0,"max":1.0,"min":0.0,"name":"critical_damage_weight","title":990,"type":"float"},
+            {"default":1.0,"max":1.0,"min":0.0,"name":"critical_damage_weight","title":992,"type":"float"},
             
-            {"default":false,"name":"elemental_mastery_use","title":234,"type":"bool"},
+            {"default":false,"name":"elemental_mastery_use","title":236,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"elemental_mastery_weight","title":235,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"elemental_mastery_weight","title":237,"type":"float"},
             
-            {"default":false,"name":"recharge_use","title":193,"type":"bool"},
+            {"default":false,"name":"recharge_use","title":195,"type":"bool"},
             
-            {"default":0.0,"max":1.0,"min":0.0,"name":"recharge_weight","title":194,"type":"float"},
+            {"default":0.0,"max":1.0,"min":0.0,"name":"recharge_weight","title":196,"type":"float"},
             
         ],
     },
