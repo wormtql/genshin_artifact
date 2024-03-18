0686ebe2ef789cf6a8b715a5058008b917e8c307
wormtql <584130248@qq.com>
2024-03-19 00:12:10 +0800

    more allows

--- mona_wasm/src/lib.rs
+++ mona_wasm/src/lib.rs
@@ -1,5 +1,17 @@
 #![allow(unused_imports)]
 #![allow(clippy::approx_constant)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unreachable_patterns)]
+#![allow(clippy::approx_constant)]
+#![allow(unused_mut)]
+#![allow(unused_assignments)]
+#![allow(dead_code)]
+#![allow(unused_macros)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unreachable_patterns)]
+#![allow(unused_must_use)]
 
 // pub mod character;
 // pub mod attribute;
