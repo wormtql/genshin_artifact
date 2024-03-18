aa5fe80b232e46754462a383f1870ea2dce5d319
wormtql <584130248@qq.com>
2024-03-19 00:23:08 +0800

    more allows

--- mona_derive/src/lib.rs
+++ mona_derive/src/lib.rs
@@ -1,3 +1,17 @@
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unreachable_patterns)]
+#![allow(clippy::approx_constant)]
+#![allow(unused_mut)]
+#![allow(unused_assignments)]
+#![allow(dead_code)]
+#![allow(unused_macros)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unreachable_patterns)]
+#![allow(unused_must_use)]
+#![allow(noop_method_call)]
+
 extern crate proc_macro;
 use proc_macro::TokenStream;
 
