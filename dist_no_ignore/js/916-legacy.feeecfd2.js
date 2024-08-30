1e8ba0e072b9ec3beb08f780dd8d55bc61d2af5a
wormtql <wormtql@users.noreply.github.com>
2024-08-30 15:09:54 +0000

    ci: Update build result

---
+++ dist_no_ignore/js/916-legacy.feeecfd2.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunkgenshin_artifacts"]=self["webpackChunkgenshin_artifacts"]||[]).push([[916,320],{74916:function(e,t,c){var s=c(82109),a=c(22261);s({target:"RegExp",proto:!0,forced:/./.exec!==a},{exec:a})}}]);
